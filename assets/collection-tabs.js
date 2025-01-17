ce2d91ac52b00da8ef64084d7cb1768ebb7695fa
mayurmodi619 <mayurmodi619@gmail.com>
2025-01-17 11:56:38 +0530

    code update

--- assets/collection-tabs.js
+++ assets/collection-tabs.js
@@ -1,41 +1,21 @@
-/* 
-var xhr = new XMLHttpRequest();
-xhr.open('GET', '/collections/all?sections=template--14594622488672__product-grid,template--14594622488672__banner');
-xhr.onload = function() {
-  if (xhr.status === 200) {
-    var sections = JSON.parse(xhr.responseText);
-      console.log('sections', sections);
-    document.getElementById('product-grid').innerHTML = sections['template--14594622488672__product-grid'];
-    document.getElementById('banner').innerHTML = sections['template--14594622488672__banner'];
-  }
-  else {
-    console.log('Request failed.  Returned status of ' + xhr.status);
-  }
-};
-xhr.send();
-*/
-
 function collectionTabsInit() {
   function renderProductGridContainer(html) {
-    console.log(html);
-    /* 
     document.getElementById('ProductGridContainer').innerHTML = new DOMParser()
       .parseFromString(html, 'text/html')
       .getElementById('ProductGridContainer').innerHTML;
-    document.getElementById('collectiontabs').innerHTML = new DOMParser().parseFromString(html, 'text/html').getElementById('collectiontabs').innerHTML;
+    document.getElementById('collectiontabs').innerHTML = new DOMParser()
+      .parseFromString(html, 'text/html')
+      .getElementById('collectiontabs').innerHTML;
     console.log(document.getElementById('collectiontabs').innerHTML);
-    */
     // document.getElementById('ProductGridContainer').querySelectorAll('.scroll-trigger').forEach((element) => {element.classList.add('scroll-trigger--cancel');});
   }
   function getFetchUrl(collectionUrl) {
-    var bannerSectionId = document.querySelector('.main-collection-banner.shopify-section').id;
-    bannerSectionId = bannerSectionId.replace('shopify-section-','');
-    var productSectionId = document.getElementById('product-grid').id;
+    var sectionId = document.getElementById('product-grid').dataset.id;
     var url = collectionUrl;
     if (window.location.search) {
-      url = collectionUrl + window.location.search + '&section_id=' + productSectionId;
+      url = collectionUrl + window.location.search + '&section_id=' + sectionId;
     } else {
-      url = url + '?section_id=' + productSectionId;
+      url = url + '?section_id=' + sectionId;
     }
     console.log('url: ', url);
     return url;
@@ -48,7 +28,6 @@ function collectionTabsInit() {
     console.log('newurl: ', newurl);
     return newurl;
   }
-
   var collectionTabLinks = document.querySelectorAll('.collection-tab-item .collection-link');
   //   console.log('length: ', collectionTabLinks.length > 0);
   if (collectionTabLinks.length > 0) {
@@ -65,11 +44,9 @@ function collectionTabsInit() {
 
         // Fetch the collection data using the URL
         const response = await fetch(url);
-        console.log(response);
+
         // Get the HTML content of the collection
         const html = await response.text();
-        console.log(html);
-        
         //console.log(html);
         renderProductGridContainer(html);
 
